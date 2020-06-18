895495792e116e729932436eba3a5b5b1521119a
cedric behr <cedric.behr57@laposte.net>
2020-06-18 18:07:23 +0200

    Adding Logs and Nationalities Class for testing purpose

---
+++ Domaine/Entities/Logs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domaine.Entities
+{
+    class Logs
+    {
+        public int Id { get; set; }
+        public int ModifId { get; set; }
+
+        [ForeignKey("ModifId"), Required]
+        virtual public Profile Modif { get; set; }
+
+        public int ProfileModifId { get; set; }
+
+        [ForeignKey("ProfileModifId"), Required]
+        virtual public Profile ProfileModif { get; set; }
+
+        public DateTime ModifDate { get; set; }
+        public string UpdateMsg { get; set; }
+
+    }
+}
---
+++ Domaine/Entities/Nationalities.cs
@@ -0,0 +1,8 @@
+﻿namespace Domaine.Entities
+{
+    class Nationalities
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
